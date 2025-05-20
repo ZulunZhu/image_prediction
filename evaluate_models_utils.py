fix train val test loss computation, fix & add threshold for early stopping, verbose gt-pred difference

evaluate_models_utils.py
@@ -33,6 +33,7 @@ def evaluate_image_link_prediction_without_dataloader(logger: str,
                                                     eval_stage: str,
                                                     eval_metric_name: str, 
                                                     evaluator: LinkPredictionEvaluator, 
+                                                    evaluate_metrics: list,
                                                     edge_raw_features: np.ndarray, 
                                                     edge_node_pairs: list,
                                                     node_mapping: pd.DataFrame,
@@ -61,8 +62,6 @@ def evaluate_image_link_prediction_without_dataloader(logger: str,
         model[0].set_neighbor_sampler(neighbor_sampler)
 
     model.eval()
-    
-    evaluate_metrics = []
 
     with torch.no_grad():
 
@@ -126,6 +125,7 @@ def evaluate_image_link_prediction_without_dataloader(logger: str,
         # Compute positive probabilities
         # sigmoid() skews the distribution while keeping values within 0 to 1
         # clamp() keeps values within 0 to 1 without changing the distribution of values between 0 and 1 
+        # Using clamp() for now to mitigate out of bounds coherence values, need to double check ReLu in link predictor to penalize negative values
         # positive_probabilities = model[1](input_1=src_node_embeddings, input_2=dst_node_embeddings).squeeze(dim=-1).sigmoid().cpu().numpy()
         positive_probabilities = model[1](input_1=src_node_embeddings, input_2=dst_node_embeddings).squeeze(dim=-1).clamp(min=0.0, max=1.0).cpu().numpy()  
         
@@ -169,10 +169,13 @@ def evaluate_image_link_prediction_without_dataloader(logger: str,
                 # Reshape ground truth and prediction into 2D
                 gt_2d = reshape_to_2d(gt_embeddings[i], H, W)
                 pred_2d = reshape_to_2d(pred_embeddings[i], H, W)
+                residual_2d = gt_2d - pred_2d
 
                 # Save results
                 np.save(save_name + "_gt.npy", gt_2d)
                 np.save(save_name + "_pred.npy", pred_2d)
+                # np.save(save_name + "_residual.npy", residual_2d)
+                # np.save(save_name + "_residualabs.npy", np.abs(residual_2d))
                 
                 # Visualize results
                 evaluate_image_link_prediction_visualiser(logger, gt_2d, pred_2d, save_name + ".png")              
@@ -189,7 +192,7 @@ def evaluate_image_link_prediction_visualiser(logger,gt_img,pred_img,save_path):
         """
         # Uncomment this line for the original code where differenced image is coerced to np.int16
         # diff = np.abs(pred_img.astype(np.int16) - gt_img.astype(np.int16))
-        diff = np.abs(pred_img - gt_img)
+        diff = np.abs(gt_img - pred_img)
         
         # Convert the predicted image to RGB
         rgb = np.stack([pred_img, pred_img, pred_img], axis=-1)
@@ -202,9 +205,13 @@ def evaluate_image_link_prediction_visualiser(logger,gt_img,pred_img,save_path):
                 f"Statistics for {save_path}\n"
                 f"[min | 1st | 25th | 50th | 75th | 99th | max]\n"
                 f"Ground truth percentiles: \n"
-                f"{np.percentile(gt_img, [0, 1, 25, 50, 75, 99, 100])}\n\n"
+                f"{np.percentile(gt_img, [0, 1, 25, 50, 75, 99, 100])}\n"
                 f"Prediction percentiles: \n"
-                f"{np.percentile(pred_img, [0, 1, 25, 50, 75, 99, 100])}\n\n")
+                f"{np.percentile(pred_img, [0, 1, 25, 50, 75, 99, 100])}\n"
+                f"Ground truth - prediction percentiles: \n"
+                f"{np.percentile(gt_img - pred_img, [0, 1, 25, 50, 75, 99, 100])}\n"
+                f"abs(ground truth - prediction) percentiles: \n"
+                f"{np.percentile(np.abs(gt_img - pred_img), [0, 1, 25, 50, 75, 99, 100])}\n")
     
     # Compute difference image
     # diff_img = highlight_differences(pred_img, gt_img, threshold=0.1)
@@ -215,12 +222,12 @@ def evaluate_image_link_prediction_visualiser(logger,gt_img,pred_img,save_path):
     axs[0].set_title("Ground Truth"); fig.colorbar(im0, ax=axs[0])
     im1 = axs[1].imshow(pred_img, cmap='gray', vmin=0, vmax=1)
     axs[1].set_title("Predicted"); fig.colorbar(im1, ax=axs[1])
-    im2 = axs[2].imshow(pred_img - gt_img, cmap='gray', vmin=-1, vmax=1)
-    axs[2].set_title(f"Difference (Pred-GT)"); fig.colorbar(im2, ax=axs[2])
+    im2 = axs[2].imshow(gt_img - pred_img, cmap='gray', vmin=-1, vmax=1)
+    axs[2].set_title(f"Difference (GT-Pred)"); fig.colorbar(im2, ax=axs[2])
     plt.tight_layout()
     plt.savefig(save_path)
     plt.close(fig)
-    logger.info(f"Saved prediction results to {save_path}")
+    logger.info(f"Saved prediction results to {save_path}\n")
 
 
 def evaluate_image_link_prediction(model_name: str, model: nn.Module, neighbor_sampler: NeighborSampler, evaluate_idx_data_loader: DataLoader, evaluate_data: Data, eval_stage: str,

image_link_prediction.py
@@ -226,11 +226,14 @@ if __name__ == "__main__":
             shutil.rmtree(save_model_folder, ignore_errors=True)
             os.makedirs(save_model_folder, exist_ok=True)
 
-            early_stopping = EarlyStopping(patience=args.patience, save_model_folder=save_model_folder,
+            early_stopping = EarlyStopping(patience=args.patience, patience_threshold=args.patience_threshold, save_model_folder=save_model_folder,
                                         save_model_name=args.save_model_name, logger=log_patch, model_name=args.model_name)
 
             loss_func = nn.BCELoss()    # BCE loss is not used as of the moment. Using L1 loss (MAE) instead.  
             evaluator = Evaluator(name=args.dataset_name)
+            
+            # Storage arrays for validation metrics
+            val_metrics, test_metrics = [], []
 
             ######### EPOCH LOOP #########
             for epoch in range(args.num_epochs):
@@ -415,6 +418,8 @@ if __name__ == "__main__":
                         # Note that edge_raw_features shape is [total no. of edges in full dataset +1, no. of pixels in patch], 
                         # where +1 refers to the first row padded with zeros, so all data in edge_raw_features[0,] should be ignored,
                         # so we +1 to train_data_indices below
+                        # Using clamp() for now to mitigate out of bounds coherence values, need to double check ReLu in link predictor to penalize negative values
+                        predicted_edge_feature = predicted_edge_feature.clamp(min=0.0, max=1.0)
                         loss = torch.nn.functional.l1_loss(predicted_edge_feature, torch.tensor(edge_raw_features[train_data_indices + 1], dtype=torch.float32, device = args.device))
                         
                         # Uncomment to visualize train results for debugging
@@ -490,6 +495,7 @@ if __name__ == "__main__":
                 
                 log_patch.info(f"********** PATCH {patch_id + 1:04d} | RUN {run + 1} | EPOCH {epoch + 1} | VALIDATION **********")
                 if epoch == 9:
+                    # Uncomment to visualize train results for debugging
                     val_metrics = evaluate_image_link_prediction_without_dataloader(logger=log_patch,
                                                                                 model_name=args.model_name,
                                                                                 model=model,
@@ -499,6 +505,7 @@ if __name__ == "__main__":
                                                                                 eval_stage='val',
                                                                                 eval_metric_name=eval_metric_name,
                                                                                 evaluator=evaluator,
+                                                                                evaluate_metrics=val_metrics,
                                                                                 edge_raw_features = edge_raw_features,
                                                                                 edge_node_pairs=edge_node_pairs,
                                                                                 node_mapping=node_mapping,
@@ -506,6 +513,7 @@ if __name__ == "__main__":
                                                                                 time_gap=args.time_gap,
                                                                                 visualize=True)
                 else:
+                    # Validate as per normal without visualizing
                     val_metrics = evaluate_image_link_prediction_without_dataloader(logger=log_patch,
                                                                                 model_name=args.model_name,
                                                                                 model=model,
@@ -515,6 +523,7 @@ if __name__ == "__main__":
                                                                                 eval_stage='val',
                                                                                 eval_metric_name=eval_metric_name,
                                                                                 evaluator=evaluator,
+                                                                                evaluate_metrics=val_metrics,
                                                                                 edge_raw_features = edge_raw_features,
                                                                                 edge_node_pairs=edge_node_pairs,
                                                                                 node_mapping=node_mapping,
@@ -527,45 +536,24 @@ if __name__ == "__main__":
                     val_backup_memory_bank = model[0].memory_bank.backup_memory_bank()
                 
                 # Logging validation loss for the specified epoch
-                log_patch.info(f"VALIDATION LOSS FOR PATCH {patch_id + 1:04d}, RUN {run + 1}, EPOCH {epoch + 1}: {val_metrics}\n\n")
+                log_patch.info(f"VALIDATION LOSS FOR PATCH {patch_id + 1:04d}, RUN {run + 1}, EPOCH {epoch + 1}: {val_metrics[-1]['val MAE loss']}\n\n")
                 
                 # Logging average training and validation losses
                 log_patch.info(f'Number of epochs: {epoch + 1}, learning rate: {optimizer.param_groups[0]["lr"]}, mean train loss: {np.mean(train_losses):.4f}')
                 for metric_name in train_metrics[0].keys():
-                    log_patch.info(f'train {metric_name}, {np.mean([train_metric[metric_name] for train_metric in train_metrics]):.4f}')
+                    log_patch.info(f'train mean {metric_name}, {np.mean([train_metric[metric_name] for train_metric in train_metrics]):.4f}')
                 for metric_name in val_metrics[0].keys():
-                    log_patch.info(f'validate {metric_name}, {np.mean([val_metric[metric_name] for val_metric in val_metrics]):.4f}')
-                import pdb; pdb.set_trace()
+                    log_patch.info(f'validate mean {metric_name}, {np.mean([val_metric[metric_name] for val_metric in val_metrics]):.4f}')
 
-                # # perform testing once after test_interval_epochs
-                # if (epoch + 1) % args.test_interval_epochs == 0:
-                #     test_metrics = evaluate_image_link_prediction(model_name=args.model_name,
-                #                                                 model=model,
-                #                                                 neighbor_sampler=full_neighbor_sampler,
-                #                                                 evaluate_idx_data_loader=test_idx_data_loader,
-                #                                                 evaluate_data=test_data,
-                #                                                 eval_stage='test',
-                #                                                 eval_metric_name=eval_metric_name,
-                #                                                 evaluator=evaluator,
-                #                                                 edge_raw_features = edge_raw_features,
-                #                                                 num_neighbors=args.num_neighbors,
-                #                                                 time_gap=args.time_gap)
-
-                #     if args.model_name in ['JODIE', 'DyRep', 'TGN']:
-                #         # reload validation memory bank for testing nodes or saving models
-                #         # note that since model treats memory as parameters, we need to reload the memory to val_backup_memory_bank for saving models
-                #         model[0].memory_bank.reload_memory_bank(val_backup_memory_bank)
-
-                #     for metric_name in test_metrics[0].keys():
-                #         logger.info(f'test {metric_name}, {np.mean([test_metric[metric_name] for test_metric in test_metrics]):.4f}')
-
-                # Select the best model based on all the validate metrics
+                # Select the best model based on all types of validate metrics
+                # For L1 MAE loss, lower is better, so use 'False' in val_metric_indicator for early stopping
                 val_metric_indicator = []
                 for metric_name in val_metrics[0].keys():
-                    val_metric_indicator.append((metric_name, np.mean([val_metric[metric_name] for val_metric in val_metrics]), True))
+                    val_metric_indicator.append((metric_name, np.mean([val_metric[metric_name] for val_metric in val_metrics]), False))
                 early_stop = early_stopping.step(val_metric_indicator, model)
 
                 if early_stop:
+                    log_patch.info(f"Early stopping at EPOCH {epoch + 1} for PATCH {patch_id + 1:04d} | RUN {run + 1}.\n")
                     break
             
             # Load the best model
@@ -588,6 +576,7 @@ if __name__ == "__main__":
                                                                             eval_stage='test_end',
                                                                             eval_metric_name=eval_metric_name,
                                                                             evaluator=evaluator,
+                                                                            evaluate_metrics=test_metrics,
                                                                             edge_raw_features = edge_raw_features,
                                                                             edge_node_pairs=edge_node_pairs,
                                                                             node_mapping=node_mapping,

utils/EarlyStopping.py
@@ -6,7 +6,7 @@ import logging
 
 class EarlyStopping(object):
 
-    def __init__(self, patience: int, save_model_folder: str, save_model_name: str, logger: logging.Logger, model_name: str = None):
+    def __init__(self, patience: int, patience_threshold: float, save_model_folder: str, save_model_name: str, logger: logging.Logger, model_name: str = None):
         """
         Early stop strategy.
         :param patience: int, max patience
@@ -16,6 +16,7 @@ class EarlyStopping(object):
         :param model_name: str, model name
         """
         self.patience = patience
+        self.patience_threshold = patience_threshold
         self.counter = 0
         self.best_metrics = {}
         self.early_stop = False
@@ -36,17 +37,20 @@ class EarlyStopping(object):
         metrics_compare_results = []
         for metric_tuple in metrics:
             metric_name, metric_value, higher_better = metric_tuple[0], metric_tuple[1], metric_tuple[2]
-
+            
+            # for each metric, if the computed metric is better than the best metric, set metrics_compare_results to True
+            # use patience_threshold for leniency in comparison 
             if higher_better:
-                if self.best_metrics.get(metric_name) is None or metric_value >= self.best_metrics.get(metric_name):
+                if self.best_metrics.get(metric_name) is None or metric_value >= self.best_metrics.get(metric_name) + self.patience_threshold:
                     metrics_compare_results.append(True)
                 else:
                     metrics_compare_results.append(False)
             else:
-                if self.best_metrics.get(metric_name) is None or metric_value <= self.best_metrics.get(metric_name):
+                if self.best_metrics.get(metric_name) is None or metric_value <= self.best_metrics.get(metric_name) - self.patience_threshold:
                     metrics_compare_results.append(True)
                 else:
                     metrics_compare_results.append(False)
+                    
         # all the computed metrics are better than the best metrics
         if torch.all(torch.tensor(metrics_compare_results)):
             for metric_tuple in metrics: